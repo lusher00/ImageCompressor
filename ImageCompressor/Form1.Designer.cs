386abbd2fa16a63954f6d8fb7d2bc4a84878b7dd
lusher00 <ryan.lush@gmail.com>
2016-02-10 13:22:04 -0500

    RLE image compressor

---
+++ ImageCompressor/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ImageCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.processBtn = new System.Windows.Forms.Button();
+            this.textBoxYdim = new System.Windows.Forms.TextBox();
+            this.textBoxXdim = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(12, 414);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(230, 36);
+            this.loadBtn.TabIndex = 0;
+            this.loadBtn.Text = "Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Black;
+            this.imageList1.Images.SetKeyName(0, "USBLogo.jpg");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(459, 369);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // processBtn
+            // 
+            this.processBtn.Location = new System.Drawing.Point(248, 414);
+            this.processBtn.Name = "processBtn";
+            this.processBtn.Size = new System.Drawing.Size(223, 36);
+            this.processBtn.TabIndex = 2;
+            this.processBtn.Text = "Process";
+            this.processBtn.UseVisualStyleBackColor = true;
+            this.processBtn.Click += new System.EventHandler(this.processBtn_Click);
+            // 
+            // textBoxYdim
+            // 
+            this.textBoxYdim.Location = new System.Drawing.Point(370, 388);
+            this.textBoxYdim.Name = "textBoxYdim";
+            this.textBoxYdim.Size = new System.Drawing.Size(100, 20);
+            this.textBoxYdim.TabIndex = 3;
+            this.textBoxYdim.TextChanged += new System.EventHandler(this.textBoxYdim_TextChanged);
+            // 
+            // textBoxXdim
+            // 
+            this.textBoxXdim.Location = new System.Drawing.Point(248, 388);
+            this.textBoxXdim.Name = "textBoxXdim";
+            this.textBoxXdim.Size = new System.Drawing.Size(100, 20);
+            this.textBoxXdim.TabIndex = 4;
+            this.textBoxXdim.TextChanged += new System.EventHandler(this.textBoxXdim_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.textBoxXdim);
+            this.Controls.Add(this.textBoxYdim);
+            this.Controls.Add(this.processBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loadBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button processBtn;
+        private System.Windows.Forms.TextBox textBoxYdim;
+        private System.Windows.Forms.TextBox textBoxXdim;
+
+    }
+}
+
---
+++ ImageCompressor/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageCompressor
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        string file;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<int> get565EncodedImageList(Bitmap img)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+
+                    Color pixel = img.GetPixel(j, i);
+
+                    byte red = (byte)((pixel.R >> 3) & 0x1F);
+                    byte green = (byte)((pixel.G >> 2) & 0x3F);
+                    byte blue = (byte)((pixel.B >> 3) & 0x1F);
+
+                    list.Add(red * 2048 + green * 32 + blue);
+                }
+            }
+
+            return list;
+        }
+
+        private string get565EncodedImageString(Bitmap img)
+        {
+            string imageString = String.Empty;
+            List<int> list = get565EncodedImageList(img);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                imageString += "0x" + list[i].ToString("X4") + ", ";
+                if ((i + 1) % 16 == 0)
+                    imageString += "\r\n";
+            }
+
+            return imageString;
+        }
+
+        private List<Tuple<int, int>> getRLETuppleList(List<int> list)
+        {
+            int r = 1;
+            int count = 0;
+            int i;
+
+            string rleString = String.Empty;
+            var tuppleList = new List<Tuple<int, int>>();
+
+            for (i = 0; i < (list.Count - 1); i++ )
+            {
+                if (list[i] == list[i + 1])
+                {
+                    r++;
+                }
+                else
+                {
+                    tuppleList.Add(new Tuple<int, int>(list[i], r));
+                    r = 1;
+                    count++;
+                }
+            }
+
+            // What do we do with the last element?
+            if(list[i - 1] == list[i])
+            {
+                // Its the same as the previous so just bump r on the last tupple
+                r++;
+                tuppleList.Add(new Tuple<int, int>(list[i], r));
+            }
+            else
+            {
+                // Its a new value
+                tuppleList.Add(new Tuple<int, int>(list[i], r));
+            }
+
+            return tuppleList;
+
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                try
+                {
+                    file = openFileDialog1.FileName;
+                    image = (Bitmap)Image.FromFile(file, true);
+                    pictureBox1.Image = image;
+                    textBoxXdim.Text = image.Width.ToString();
+                    textBoxYdim.Text = image.Height.ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void processBtn_Click(object sender, EventArgs e)
+        {
+            var tuppleList = new List<Tuple<int, int>>();
+            List<int> list;
+
+            try
+            {
+                list = get565EncodedImageList(image);
+                tuppleList = getRLETuppleList(list);
+
+                //Console.WriteLine(get565EncodedImageString(image));
+
+                using (StreamWriter outputFile = new StreamWriter(file + ".rle"))
+                {
+                    foreach (var idx in tuppleList)
+                        outputFile.WriteLine("{0}, {1},", "{0x" + idx.Item1.ToString("X4"), "0x" + idx.Item2.ToString("X4") + "}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Success! " + tuppleList.Count.ToString() + " elements");
+            }
+        }
+
+        private void textBoxXdim_TextChanged(object sender, EventArgs e)
+        {
+            if(image != null)
+            {
+                    
+            }
+        }
+
+        private void textBoxYdim_TextChanged(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                //graphics.CompositingMode = CompositingMode.SourceCopy;
+                //graphics.CompositingQuality = CompositingQuality.HighQuality;
+                //graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //graphics.SmoothingMode = SmoothingMode.HighQuality;
+                //graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
